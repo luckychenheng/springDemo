5937475a5142245709897502bb5c838de488072c
lucky <seemac@seedeMacBook-Pro.local>
2019-05-09 10:01:58 +0800

    no message

--- SpringCloudLearning-master/chapter2/service-ribbon/src/main/java/com/forezp/web/HelloControler.java
+++ SpringCloudLearning-master/chapter2/service-ribbon/src/main/java/com/forezp/web/HelloControler.java
@@ -1,6 +1,8 @@
 package com.forezp.web;
 
 import com.forezp.service.HelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloControler {
 
+    static int connectNumber;
 
+    protected static final Logger logger = LoggerFactory.getLogger(HelloControler.class);
 
     @Autowired
     HelloService helloService;
 
     @RequestMapping(value = "/hi")
     public String hi(@RequestParam String name){
+        connectNumber ++;
+        logger.info("connectNumber ===> " + connectNumber);
         return helloService.hiService(name);
     }
 
